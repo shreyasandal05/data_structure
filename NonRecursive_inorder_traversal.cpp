65a8d41dd17d21184b9c76c1077ec487dab8eb1f
Shreya Sandal <sandalshreya@gmail.com>
2021-01-30 20:36:38 +0530

    Added Non Recursive Inorder Traversal

---
+++ NonRecursive_inorder_traversal.cpp
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stack>
+#include<stdlib.h>
+#include<iostream>
+#define MAXSTK 5
+
+using namespace std;
+
+struct BinaryTreeNode{
+	int data;
+	struct BinaryTreeNode*left;
+	struct BinaryTreeNode*right;
+	BinaryTreeNode(int data){
+		this->data=data;
+		left=right=NULL;
+	}
+};
+
+struct BinaryTreeNode* insert(struct BinaryTreeNode* root,int data){
+    if(root==NULL){
+        struct BinaryTreeNode* node=(struct BinaryTreeNode*)malloc(sizeof(struct BinaryTreeNode));
+        node->data=data;
+        node->left=NULL;
+        node->right=NULL;
+        return node;
+    }
+    else
+    {
+        struct BinaryTreeNode* newnode;
+        if(data<=root->data){
+            newnode=insert(root->left,data);
+            root->left=newnode;
+        }
+        else
+            {
+                newnode=insert(root->right,data);
+                root->right=newnode;
+            }
+        return root;
+    }
+    
+}
+
+
+void inOrder(struct BinaryTreeNode* root){
+	stack<BinaryTreeNode*> st;
+	BinaryTreeNode* current=root;
+	while(current!=NULL||st.empty()==false){
+		while(current!=NULL){
+			st.push(current);
+			current=current->left;
+		}
+		current=st.top();
+		st.pop();
+		cout<<current->data<<" ";
+		current=current->right;
+	}
+}
+
+int main(){
+	int t;
+	cin>>t;
+	struct BinaryTreeNode* root=NULL;
+    int data;
+	while(t--){
+		scanf("%d",&data);
+        root=insert(root,data);
+	}	
+	inOrder(root);
+	return 0;
+}
+
+
+
